e4559dcf1d66ce15463a2645c92a730d86c70b72
Pranshu Patel <77317824+RuleBreaker-99@users.noreply.github.com>
2026-01-30 17:51:34 +0530

    Add email notification message to test.py

--- test.py
+++ test.py
@@ -1,3 +1,4 @@
 print("This is an internship project")
 print("Banking ATM System")
-print(These changes are for testing Poll SCM")
+print("These changes are for testing Poll SCM")
+print("Email notification configuration done")
